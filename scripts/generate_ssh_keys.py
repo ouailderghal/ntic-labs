a6b68392ac401411f3b773e681918d3816441864
Ouail Derghal <ouailderghal1@gmail.com>
2022-08-10 13:56:42 +0100

    Update SSH keys generator script: add flag to generate authorized keys file

--- scripts/generate_ssh_keys.py
+++ scripts/generate_ssh_keys.py
@@ -2,12 +2,14 @@
 
 import argparse
 from pathlib import Path
+import contextlib
 
 from cryptography.hazmat.primitives import serialization as crypto_serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.backends import default_backend as crypto_default_backend
 
 DEFAULT_NUMBER_OF_KEYS = 5
+BASE_KEY_FILE_NAME = 'id_rsa_admin'
 
 parser = argparse.ArgumentParser(description='Generate SSH key pairs for administrators.')
 
@@ -32,10 +34,17 @@ parser.add_argument('-d, --destination',
 parser.add_argument('-g',
                     help='generate authorized keys',
                     required=False,
-                    default=False)
+                    default=False,
+                    action='store_true',
+                    dest='generate_authorized_keys')
 
 args = parser.parse_args()
 export_destination = Path(args.destination)
+
+f_authorized_keys = Path(export_destination.joinpath('authorized_keys'))
+
+with contextlib.suppress(FileNotFoundError):
+    f_authorized_keys.unlink()
 
 for (i, _) in enumerate(range(args.number_of_keys)):
     key = rsa.generate_private_key(
@@ -55,16 +64,20 @@ for (i, _) in enumerate(range(args.number_of_keys)):
         crypto_serialization.PublicFormat.OpenSSH
     )
 
-    f_private_key = Path(export_destination.joinpath(f'admin_{i + 1}.priv'))
-    f_public_key = Path(export_destination.joinpath(f'admin_{i + 1}.pub'))
+    f_private_key = export_destination.joinpath(f'{BASE_KEY_FILE_NAME}_{i + 1}.priv')
+    f_public_key = export_destination.joinpath(f'{BASE_KEY_FILE_NAME}_{i + 1}.pub')
 
-    with open(f_private_key, 'w+') as f_priv:
-        f_priv.write(str(private_key.decode()))
-        f_priv.write('\n')
+    with open(f_private_key, mode='w') as f_priv:
+        private_key_string = f'{private_key.decode()}\n'
+        f_priv.write(private_key_string)
 
-    with open(f'admin_{i + 1}.pub', 'w+') as f_pub:
-        f_pub.write(str(public_key.decode()))
-        f_pub.write('\n')
+    with open(f_public_key, mode='w') as f_pub:
+        public_key_string = f'{public_key.decode()}\n'
+        f_pub.write(public_key_string)
+
+        if args.generate_authorized_keys:
+            with open(f_authorized_keys, mode='a') as f_ak:
+                f_ak.write(f'{public_key_string}')
 
     # Update permissions on key files
     [key.chmod(0o600) for key in (f_private_key, f_public_key)]
