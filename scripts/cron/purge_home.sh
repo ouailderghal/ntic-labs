ca96d9e0f8b6176a5503ea690f3c331f121d688d
Ouail Derghal <ouailderghal1@gmail.com>
2022-08-26 12:02:15 +0100

    Add cron scripts

---
+++ scripts/cron/purge_home.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# 
+# Purge guest HOME directory and copy default XFCE configuration files
+#
+
+rm -rf /home/guest/*
+rm -rf /home/guest/Desktop
+rm -rf /home/guest/.config
+rm -Rf /home/guest/.*
+mkdir -p /home/guest/.config/
+cp -r /opt/xfce4 /home/guest/.config/
+chown -R guest:guest /home/guest/.config
