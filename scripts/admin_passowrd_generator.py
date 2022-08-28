9014f743b5e95fbb4131dd38868625c44a3c854b
Ouail Derghal <ouailderghal1@gmail.com>
2022-08-28 15:00:06 +0100

    Add admin passwords

--- scripts/admin_passowrd_generator.py
+++ scripts/admin_passowrd_generator.py
@@ -29,25 +29,38 @@ def main() -> None:
     """
     args = get_args()
     alphabet = string.ascii_lowercase + string.digits
+    digits = string.digits
 
     f_inventory = Path(args.inventory)
     passwords_file = Path('./passwords.csv')
 
+    with open(passwords_file, 'w') as f_passwords:
+        f_passwords.write('ip_address,bios_password,plain_password,hashed_password\n')
+
     with open(f_inventory) as f:
         inventory = yaml.load(f, Loader=yaml.SafeLoader)
-        labs = list(inventory['all']['children']['machines']['children'].keys())
-        # hosts = [inventory['all']['children']['machines']['children'][lab]['hosts'] for lab in labs]
 
-        for lab in labs:
-            hosts = [inventory['all']['children']['machines']['children'][lab]['hosts'] for lab in labs]
+        labs = {
+            'lab4': inventory['all']['children']['machines']['children']['lab4']['hosts'],
+            'lab5': inventory['all']['children']['machines']['children']['lab5']['hosts'],
+            'lab6': inventory['all']['children']['machines']['children']['lab6']['hosts'],
+        }
 
-            for host in hosts:
-                print(json.dumps(host.key(), indent=2))
+        for lab in labs.values():
+            for host in lab:
+                plain_password = ''.join(secrets.choice(alphabet) for _ in range(8))
+                bios_password = ''.join(secrets.choice(digits[1:]) for _ in range(4))
+                hashed_password = crypt.crypt(plain_password)
+                lab[host]['admin_password'] = hashed_password
 
-        # for host in hosts:
-        #     plain_password = ''.join(secrets.choice(alphabet) for i in range(12))
-        #     hashed_password = crypt.crypt(plain_password)
-        #     hosts[host]['admin_password'] = hashed_password
+                with open(passwords_file, 'a') as f_passwords:
+                    f_passwords.write(f"{host},{bios_password},{plain_password},{hashed_password}\n")
+
+        for lab in labs.keys():
+            inventory['all']['children']['machines']['children'][lab]['hosts'] = labs[lab]
+
+        with open('new_inventory.yml', 'w') as new_inventory:
+            yaml.dump(inventory, new_inventory, default_flow_style=False)
 
 
 if __name__ == '__main__':
