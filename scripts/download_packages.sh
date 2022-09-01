553f081089b096e854a5588291a220d607d3ebea
Ouail Derghal <ouailderghal1@gmail.com>
2022-09-01 17:25:41 +0100

    Update package download script

--- scripts/download_packages.sh
+++ scripts/download_packages.sh
@@ -19,26 +19,23 @@ WORKDIR=$HOME/Projects/lmc
 DOWNLOAD_DIR=$WORKDIR/packages
 
 ECLIPSE_URL="https://ftp.osuosl.org/pub/eclipse/technology/epp/downloads/release/2022-06/R/eclipse-java-2022-06-R-linux-gtk-x86_64.tar.gz"
-ECLIPSE_FILENAME="eclipse.tar.gz"
+ECLIPSE_FILENAME="$WORKDIR/roles/java_lab/files/eclipse.tar.gz"
 
 VSCODE_URL="https://az764295.vo.msecnd.net/stable/da76f93349a72022ca4670c1b84860304616aaa2/code_1.70.0-1659589288_amd64.deb"
-VSCODE_FILENAME="vscode.deb"
+VSCODE_FILENAME="$WORKDIR/roles/web_lab/files/vscode.deb"
 
 PACKETTRACER_URL="https://ia802208.us.archive.org/20/items/pt81_20220222/CiscoPacketTracer_811_Ubuntu_64bit.deb"
-PACKETTRACER_FILENAME="packettracer.deb"
-
-# Create download directory if it does not exist
-mkdir -p $DOWNLOAD_DIR 
+PACKETTRACER_FILENAME="$WORKDIR/roles/networking_lab/files/packettracer.deb"
 
 # Download Eclipse for Java Developers
-[ ! -f $DOWNLOAD_DIR/$ECLIPSE_FILENAME ] && curl $ECLIPSE_URL -o $DOWNLOAD_DIR/$ECLIPSE_FILENAME || \
+[ ! -f $ECLIPSE_FILENAME ] && curl $ECLIPSE_URL -o $ECLIPSE_FILENAME || \
     echo "[INFO] Eclipse for Java Developers has already been downloaded."
 
 # Download Microsoft Visual Studio Code
-[ ! -f $DOWNLOAD_DIR/$VSCODE_FILENAME ] && curl $VSCODE_URL -o $DOWNLOAD_DIR/$VSCODE_FILENAME || \
+[ ! -f $VSCODE_FILENAME ] && curl $VSCODE_URL -o $VSCODE_FILENAME || \
     echo "[INFO] Visual Studio Code has already been downloaded."
 
 # Download Cisco Packet Tracer
-[ ! -f $DOWNLOAD_DIR/$PACKETTRACER_FILENAME ] && curl $PACKETTRACER_URL -o $DOWNLOAD_DIR/$PACKETTRACER_FILENAME || \
+[ ! -f $PACKETTRACER_FILENAME ] && curl $PACKETTRACER_URL -o $PACKETTRACER_FILENAME || \
     echo "[INFO] Cisco Packet Tracer has already been downloaded."
 
